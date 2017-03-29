12610263c3997f7232bc16fdc651760555a753f3
radwasherif <radwasherif3@gmail.com>
2017-03-30 00:35:39 +0200

    both event types in one file

--- app/models/Event.js
+++ app/models/Event.js
@@ -1,8 +1,28 @@
+var mongoose = require('mongoose');
+
+// defining schemas 
+
+/**
+ * PermanentEvent is an event that is available at any given point in time as an essential part of the business's 
+ * operation
+ */
+
+var permanentEventSchema = mongoose.Schema({
+    name: String,
+    description: String,
+    location: String,
+    price: Double,
+    capacity: Integer,
+    datetime: Date
+
+});
+
+
 /**Special events are events that are not made available permanently by the business 
  * They can be events that are held on a weekly, monthly, annual basis, or held at arbitrary times. 
  * repition_type accounts for which pattern the business follows
  */
-var mongoose = require('mongoose');
+
 var specialEventSchema = mongoose.Schema({
     name: String,
     description: String,
@@ -14,6 +34,12 @@ var specialEventSchema = mongoose.Schema({
 
 });
 
-var SpecialEvent = mongoose.model("specialEvent", specialEventSchema);
 
-module.exports = SpecialEvent;  
+//creating models
+var SpecialEvent = mongoose.model("specialEvent", specialEventSchema);
+var PermanentEvent = mongoose.model("permanentEvent", permanentEventSchema);
+
+
+//exporting models
+module.exports = PermanentEvent;  
+module.exports = SpecialEvent;
