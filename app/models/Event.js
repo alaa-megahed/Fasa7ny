a5a9e50123b8c6eee54dbad548b9bde082a310fb
alaa-megahed <alaamogahed2014@gmail.com>
2017-03-30 09:46:29 +0200

    added images and available, fixed permanent and special events, fixed double

--- app/models/Event.js
+++ app/models/Event.js
@@ -5,33 +5,36 @@ var mongoose = require('mongoose');
 /**
  * PermanentEvent is an event that is available at any given point in time as an essential part of the business's 
  * operation
+ * They can be events that are held on a weekly, monthly, annual basis, or held at arbitrary times.
+ * repition_type accounts for which pattern the business follows
  */
 
 var permanentEventSchema = mongoose.Schema({
     name: String,
     description: String,
     location: String,
-    price: Double,
-    capacity: Integer,
-    timing: Date
-
+    price: SchemaTypes.Double,
+    capacity: Number,
+    available: Number,
+    repition_type: Integer, // to be changes to repition_pattern 
+    timing: [Date],
+    images: [String]
 });
 
 
 /**Special events are events that are not made available permanently by the business 
- * They can be events that are held on a weekly, monthly, annual basis, or held at arbitrary times. 
- * repition_type accounts for which pattern the business follows
+    They don't have to be repeated
  */
 
 var specialEventSchema = mongoose.Schema({
     name: String,
     description: String,
     location: String,
-    price: Double,
-    capacity: Integer,
-    repition_type: Integer, // to be changes to repition_pattern 
-    timing: [Date]
-
+    price: SchemaTypes.Double,
+    capacity: Number,
+    available: Number,
+    timing: Date,
+    images: [String]
 });
 
 
