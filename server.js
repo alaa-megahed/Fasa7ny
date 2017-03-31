cfa66e4f37b59ef086eb3dfa3eceb527ad6c8d21
alaa-megahed <alaamogahed2014@gmail.com>
2017-03-31 16:41:00 +0200

    local strategy added

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var mongoose = require('./config/mongoose');
     config = require('./config/config'),
     express = require('./config/express'),
-    passport = require('passport'),//this was ./config/passport where passport.js had serialize and deserialize user, sould be updated when that file is done.
+    passport = require('passport'),
     db = mongoose(),
-  //  passport = passport(),
     app = express();
+
+
+
 
 
 
