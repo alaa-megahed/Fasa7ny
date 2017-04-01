7248d2b14d0a4df40a3869541e9605206bcfc69b
radwasherif <radwasherif3@gmail.com>
2017-04-01 10:48:56 +0200

    sort and filter done

--- app/controllers/search.controller.js
+++ app/controllers/search.controller.js
@@ -13,59 +13,73 @@ SearchController = {
     }
     ,
     /** Searches businesses by keyword inserted by user 
+     * Filters by category, minimum rating, area 
+     * Sorts by average_rating 
      */
     search: function (req, res) {
         var formData = req.body;
-        var keyword = formData.keyword;
-        var category = formData.category;
-        var area = formData.area;
-        var minRating = parseFloat(formData.minRating);
         var sortBy = formData.sortBy;
-        console.log(keyword);
-        var query = {
-            $or: [
-                { category: new RegExp(category, 'i') },
-                { area: new RegExp(area, 'i') },
-                { average_rating: { $gte: [minRating] } }
-            ]
-        };
-        //add keyword to the query only if it's not empty
-        if (keyword.length > 0) {
-            query["$text"] = { $search: keyword };
+
+        var queryBody = helper.makeQuery(formData); // retrieve query body 
+
+        var query = Business.find(queryBody);
+
+        if (sortBy !== 'udefined' && sortBy.length > 0) { //apply sort filter only if not empty 
+            var sortObj = {}; 
+            sortObj[sortBy] = -1;
+            query.sort(sortObj);
+
         }
-        Business.find(query).
-            sort('-' + sortBy).
-            select('name').
-            exec(function (err, result) {
-                if (err)
-                    res.send(err);
-                else {
-                    res.send(result);
-                }
-            });
+
+
+        query.exec(function (err, result) {
+            if (err)
+                res.send(err);
+            else {
+                res.send(result);
+            }
+        });
 
     }
 }
 
 //module that contains helper methods to the SearchController
 helper = {
+    /**
+     * Extracts the relevent information from formData input 
+     * and returns the body of the query to be executed 
+     */
     makeQuery: function (formData) {
-        var formData = req.body;
-        var keyword = formData.keyword;
-        var category = formData.category;
-        var area = formData.area;
-        var minRating = parseFloat(formData.minRating);
-        var sortBy = formData.sortBy;
+    
+        var keyword = formData.keyword || ""; //text search index over name, description and area (no substring)
+        var category = formData.category || ""; 
+        var area = formData.area || "";
+        var minRating = formData.minRating || "";
+       
+        var query = {};
         if (keyword.length > 0) {
             query["$text"] = { $search: keyword };
         }
-        var oring = []; 
-        if(category.length > 0) {
-            oring.push({category: new RegExp(category, 'i')}); 
+        /**
+         * This array could be changes to or, and or a combinaring of both 
+         */
+        var anding = [];
+        if (category.length > 0) {
+            anding.push({ category: new RegExp(category, 'i') });
         }
-        if(area.length > 0) {
-            oring.push({area: new RegExp(area, 'i')});
+        if (area.length > 0) {
+            anding.push({ area: new RegExp(area, 'i') });
         }
+        if (minRating.length > 0) {
+            var minRatingFloat = parseFloat(minRating); 
+            console.log(minRatingFloat); 
+            anding.push({ average_rating: { $gte: minRatingFloat} });
+        }
+        if (anding.length > 0) {
+            query["$and"] = anding;
+        }
+        console.log(query);
+        return query;
     }
 }
 module.exports = SearchController; 
