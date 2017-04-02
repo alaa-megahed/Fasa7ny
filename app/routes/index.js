90cd49fa30e1b4fb3805da203693a8e4e4497fd1
radwasherif <radwasherif3@gmail.com>
2017-04-02 09:20:11 +0200

    added contact route

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,6 +7,9 @@ var business = require('./business.routes.js');
 // router.use('/search', search); 
 router.use('/search', search);  
 router.use('/business', business); 
+router.use('/contact', function(req, res) {
+    res.send('Contact us with a business proposal at fasa7ny@gmail.com'); 
+}); 
 router.get('/', function (req, res) {
     res.send("Hello");
 }); 
