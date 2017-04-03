c30f30a2b75b76c0f2e6efc8407c14d24b4fb6c2
habiibaelghamry <habiiba.elghamry@gmail.com>
2017-04-03 20:25:05 +0200

    changed name of business controller

--- app/controllers/business.controller.js
+++ app/controllers/business.controller.js
@@ -10,6 +10,20 @@ var BusinessController = {
                 else
                     res.send(result);
             });
+    }, 
+    equestRemoval = function(req,res) {
+        // if(req.user){
+        var id = req.query.id;
+        Business.findByIdAndUpdate(id,{$set:{deleted:1}}, function(err,business){
+            if(err) console.log("error in request removal");
+            else console.log("Requested!");
+        });
+
+        // }
+
+        // else{
+        //  console.log('not logged in');
+        // }
     }
 }
 module.exports = BusinessController; 
