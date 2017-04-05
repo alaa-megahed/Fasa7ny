6a10fcf676f205e7a19480e543fb24ecc6698ab8
yasmineamr <yasmineabdulfattah@gmail.com>
2017-04-06 01:07:01 +0200

    modify business routes

--- app/routes/business.routes.js
+++ app/routes/business.routes.js
@@ -14,12 +14,12 @@ business.use('/makepagepublic', function(req, res) {
 business.get('/', function(req, res){
         res.sendFile(path.resolve('app/views/business.html'));
     });
-business.get('/:name', businessController.getBusiness); 
+business.get('/:name', businessController.getBusiness);
 business.post('/requestRemoval',businessController.requestRemoval)
 business.post('/deletePaymentMethod', businessController.deletePaymentMethod);
 business.post('/editInformation', businessController.editInformation);
 business.post('/deletePhone', businessController.deletePhone);
-
+business.post('/publicPage', businessController.makePagePublic);
 
 
 
