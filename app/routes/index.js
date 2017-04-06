7a147c24b5ae57c770fe695ea401a65b293fd7e2
alaa-megahed <alaamogahed2014@gmail.com>
2017-04-06 11:12:07 +0200

    resolving more conflicts

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,10 +11,7 @@ var reviews = require('./reviews.routes');
 var offers = require('./offers.routes');
 var event = require('./event.routes');
 var auth = require('./auth.routes');
-<<<<<<< HEAD
-=======
 var user = require('./RegisteredUserRouter'); 
->>>>>>> c328b15fd48fd3dc55f32a03c487eec8f31e1813
 var admin = require('./WebAdminRouter');
 
 router.use('/auth', auth);
@@ -25,13 +22,8 @@ router.use('/bookings', bookings);
 router.use('/search', search);
 router.use('/business', business);
 router.use('/event',event);
-<<<<<<< HEAD
-router.use('/admin', admin);
-=======
-router.use('/auth', auth);
 router.use('/user', user);
 router.use('/admin', admin); 
->>>>>>> c328b15fd48fd3dc55f32a03c487eec8f31e1813
 router.use('/contact', function(req, res) {
     res.send('Contact us with a business proposal at fasa7ny@gmail.com');
 });
