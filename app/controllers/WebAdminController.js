a5a3882a5391ebe2813afef29052088077e8620f
alaa-megahed <alaamogahed2014@gmail.com>
2017-04-06 10:36:09 +0200

    web admin conflicts resolved

--- app/controllers/WebAdminController.js
+++ app/controllers/WebAdminController.js
@@ -2,17 +2,12 @@ var WebAdmin  = require('mongoose').model('WebAdmin');
 var Business  = require('mongoose').model('Business');
 var generator = require('generate-password');
 var Events = require('mongoose').model('Events');
-<<<<<<< HEAD
 var Offer = require('mongoose').model('Offer');
 var User = require('mongoose').model('RegisteredUser');
 var Review = require('mongoose').model('Review');
 var Reply   = require('mongoose').model('Reply');
 var   async = require('async');
-=======
-var User   = require('mongoose').model('RegisteredUser');
-var Review = require('mongoose').model('Review');
-var Reply  = require('mongoose').model('Reply');
->>>>>>> c328b15fd48fd3dc55f32a03c487eec8f31e1813
+
 const nodemailer = require('nodemailer');
 var configAuth = require('../../config/auth');
 
@@ -31,7 +26,7 @@ exports.AddBusiness = function (req, res) {
         length: 10,
         numbers: true
     });
-<<<<<<< HEAD
+
     business.local.password = business.generateHash(generatedPassword);
     business.local.username = req.body.username;
     business.name = req.body.name;
@@ -39,27 +34,7 @@ exports.AddBusiness = function (req, res) {
     business.category = req.body.category;
     business.email= req.body.email;
     
-            business.save(function (err) {
-=======
-    var hashedPassword = generateHash(generatedPassword);
-    Business.findOne({ username: username }, function (err, user) {
-        if (err) { return next(err); }
-        if (business) {
-            res.send("user already exist");
-        }
-        else {
-            var business = new Business(
-                {
-                    name: req.body.name,
-                    username: username,
-                    password: hashedPassword,
-                    merchant_ID: req.body.merchant_ID,
-                    category: req.body.category
-
-                });
-
-            business.save(function (err, business) {
->>>>>>> c328b15fd48fd3dc55f32a03c487eec8f31e1813
+    business.save(function (err) {
 
                 if (err)
                     throw err;
@@ -79,7 +54,7 @@ exports.AddBusiness = function (req, res) {
             user: configAuth.gmail.user,
               pass: configAuth.gmail.pass
         }
-<<<<<<< HEAD
+
       });
 
         var mailOptions = {
@@ -103,45 +78,18 @@ exports.AddBusiness = function (req, res) {
     
         }
        
-=======
-    });
-
-}
-
-// exports.WebAdminDeleteUser = function(req,res){
-//     var username = req.body.username;
-//         User.remove({username:username}, function(err,user){
-//             if(err) res.send('could not remove user');
-//             else{
-//                 Review.remove({user:user}, function(err){
-//                     if(err) res.send('could not remove user');
-//                 });
-                
-
-//             }
-//         });
-
-// }
-
-
-
->>>>>>> c328b15fd48fd3dc55f32a03c487eec8f31e1813
 
 //i am not sure about attribute name deleted ? ,,, and business attribute name in offer... check eno m3ndosh booking
 
 exports.WebAdminDeleteBusiness = function (req, res) {
 
-<<<<<<< HEAD
+
 Business.findByIdAndRemove(req.params.id, function (err,business){
             if(err)
                 throw err;
            
               Offer.remove({ business: business._id }, function (err) {
-=======
-    Business.findByIdAndRemove(req.params.name, function (err, business) {
-        if (err) throw err;
-        Offer.remove({ business: req.params.name }, function (err) {
->>>>>>> c328b15fd48fd3dc55f32a03c487eec8f31e1813
+
             if (err) throw err;
         });
 
@@ -160,13 +108,9 @@ Business.findByIdAndRemove(req.params.id, function (err,business){
 
 
 exports.webAdminViewRequestedDelete = function (req, res) {
-<<<<<<< HEAD
+
     Business.find({ delete: 1 }, function (err, requests) {
         res.render('requestedDelete', {user: req.user, requests: requests});
-=======
-    Business.find({ deleted: 1 }, function (err, requests) {
-        res.render('delete_requests', requests);
->>>>>>> c328b15fd48fd3dc55f32a03c487eec8f31e1813
 
     }
   );
--- app/routes/WebAdminRouter.js
+++ app/routes/WebAdminRouter.js
@@ -9,15 +9,11 @@ var router = express.Router();
     //});
 
      
-<<<<<<< HEAD
+
      router.get('/viewRequestedDelete', webAdmin.webAdminViewRequestedDelete);
      router.get('/deleteBusiness/:id', webAdmin.WebAdminDeleteBusiness);
      router.post('/add_business', webAdmin.AddBusiness);
-=======
-router.get('/viewRequestedDelete', webAdmin.webAdminViewRequestedDelete);
-router.get('/deleteBusiness/:username', webAdmin.WebAdminDeleteBusiness);
-router.post('/add_business', webAdmin.AddBusiness);
->>>>>>> c328b15fd48fd3dc55f32a03c487eec8f31e1813
+
 
   
 
