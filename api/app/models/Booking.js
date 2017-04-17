e56ae0f3d1e43aede2175b973da5b682c743a468
alaa-megahed <alaamogahed2014@gmail.com>
2017-04-17 19:57:16 +0200

    double issue fixed

--- api/app/models/Booking.js
+++ api/app/models/Booking.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
 var SchemaTypes = mongoose.Schema.Types;
+require('mongoose-double')(mongoose);
 
 var BookingSchema = new Schema({
     booking_date : Date,
@@ -8,7 +9,7 @@ var BookingSchema = new Schema({
     booker       : {type: mongoose.Schema.Types.ObjectId, ref: 'RegisteredUser'},
     event_id     : {type: mongoose.Schema.Types.ObjectId, ref:'EventOccurrences'},
     //payment goes here
-    charge       : Number
+    charge       : SchemaTypes.Double
 });
 
 var Booking = mongoose.model('Booking', BookingSchema);
