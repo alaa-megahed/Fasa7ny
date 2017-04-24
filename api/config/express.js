5acfedc73e59d9c8f6eda6a945386582fee0b3ec
Farida Shafik <faridashafik@gmail.com>
2017-04-24 22:03:52 +0200

    req headers origin

--- api/config/express.js
+++ api/config/express.js
@@ -20,11 +20,11 @@ module.exports = function() {
       // var allowedOrigins = [null,'http://54.187.92.64:8000', 'http://54.187.92.64:8000', 'http://54.187.92.64:3000', 'http://54.187.92.64:3000', 'https://www.facebook.com'];
       // var origin = req.headers.origin;
       // if(allowedOrigins.indexOf(origin) > -1){
-        res.setHeader('Access-Control-Allow-Origin', "*");
+        res.setHeader('Access-Control-Allow-Origin', "req.headers.origin");
       // }
         res.setHeader('Access-Contro-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
         res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Accept, X-HTTP-Method-Override");
-      //  res.setHeader('Access-Control-Allow-Credentials', true);
+        res.setHeader('Access-Control-Allow-Credentials', true);
         next();
     });
 
